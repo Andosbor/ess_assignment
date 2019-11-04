bcd50bdfee2fd90b891b4ebe89e1c9417a7491cb
Oogapooki <andrewosborn101@gmail.com>
2019-11-04 09:54:11 -0600

    added deletion functionality

--- src/App.js
+++ src/App.js
@@ -34,7 +34,16 @@ class App extends Component {
     .catch(err => console.error(err))
   }
 
-  renderCourse = ({course_id, name, description }) => <div key={course_id}>{name}  |  {description}</div>
+
+  //stuck on delete
+  deleteCourse = _ => {
+    const { course } = this.state;
+    fetch(`http://localhost:4000/courses/delete?name=${course.name}`)
+    .then(this.getCourses)
+    .catch(err => console.error(err))
+  }
+
+  renderCourse = ({course_id, name, description }) => <div key={course_id}>{name}  |  {description} </div>
 
   render(){
     const { courses, course } = this.state;
@@ -42,13 +51,16 @@ class App extends Component {
       <div className="App">
           <h2>{this.state.title}</h2>
           {courses.map(this.renderCourse)}
+          <br/>
           <div>
             <input placeholder="course name" value={course.name}
              onChange={e => this.setState({ course: { ...course, name: e.target.value}})} />
             <input placeholder="course description" value={course.description} 
              onChange={e => this.setState({ course: { ...course, description: e.target.value}})}/>
              <button onClick={this.addCourse}> Add Course </button>
+             <button onClick={this.deleteCourse}> Delete Course </button>
           </div>
+        
       </div>
     );
   }
